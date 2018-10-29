9f8462d91e8cca3c0b1107368a537bb69ff86e8b
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2018-10-29 22:44:56 +0100

    [add] some more standart imports (lazy)

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
+# standart imports
 from .praktiplot import plt
 from .tablehandler import TableHandler
-__all__ = ["plt", "TableHandler"]
+import numpy as np
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from pint import UnitRegistry
+unit = UnitRegistry()
+noms = unp.nominal_values
+stds = unp.std_devs
+
+__all__ = ["plt", "TableHandler", "np", "ufloat", "unp", "unit", "noms", "stds"]
