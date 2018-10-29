9bc34d46d59454ba5d5a910cf50c3e71c066933b
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2018-10-29 23:07:27 +0100

    [add] standart import units

--- __init__.py
+++ __init__.py
@@ -9,4 +9,14 @@ unit = UnitRegistry()
 noms = unp.nominal_values
 stds = unp.std_devs
 
+# standart import useful (SI) units
+meter       = unit.meter
+seconds     = unit.seconds
+kilogram    = unit.kilogram
+kelvin      = unit.kelvin
+celcius     = unit.celsius
+
+units = ["meter", "seconds", "kilogram", "kelvin", "celsius"]
+
 __all__ = ["plt", "TableHandler", "np", "ufloat", "unp", "unit", "noms", "stds"]
+__all__ += units
