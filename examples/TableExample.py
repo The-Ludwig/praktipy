e04b2ef6a65a63147025f5182310b8ad799c7d55
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2018-11-10 18:13:07 +0100

    [upd] example

--- examples/TableExample.py
+++ examples/TableExample.py
@@ -1,13 +1,13 @@
 # you can use this program as an example on how to use this library
 
 # import the TableHandler Class
-from praktipy import TableHandler
+from praktipy import TableHandler, genfromtxt
 
 # Parse in the Table
-th = TableHandler("exampleTable.txt")
+th = genfromtxt("exampleTable.txt")
 
 # Print the mean values with errors
 print(th.getMeanValues())
 
 # Generate a tex table
-th.makeTexTable("example.tex")
+th.makeTexTable("example.tex", useSIUnitX=True, precision=10)
