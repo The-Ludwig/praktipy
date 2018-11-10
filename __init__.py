f57c72a7032fb51bf8a775e6ec7232e2080d8bca
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2018-11-10 14:40:56 +0100

    [add] scipy.constants

--- __init__.py
+++ __init__.py
@@ -5,6 +5,8 @@ import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
 from pint import UnitRegistry
+from scipy import constants as const
+
 unit = UnitRegistry()
 noms = unp.nominal_values
 stds = unp.std_devs
@@ -18,5 +20,5 @@ celsius     = unit.celsius
 
 units = ["meter", "seconds", "kilogram", "kelvin", "celsius"]
 
-__all__ = ["plt", "TableHandler", "np", "ufloat", "unp", "unit", "noms", "stds"]
+__all__ = ["plt", "TableHandler", "np", "ufloat", "unp", "unit", "noms", "stds", "const"]
 __all__ += units
