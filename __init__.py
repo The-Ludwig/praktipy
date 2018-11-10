beacdb6e137a4e8ce52bcb67c5103b86d008364e
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2018-11-10 18:03:44 +0100

    [add] convenience func and SIunitX support
    * TableHandler(filename) is no genfromtxt

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # standart imports
 from .praktiplot import plt
-from .tablehandler import TableHandler
+from .tablehandler import TableHandler, genfromtxt
 import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
@@ -18,7 +18,26 @@ kilogram    = unit.kilogram
 kelvin      = unit.kelvin
 celsius     = unit.celsius
 
+
+# convenience function
+
+def polyplotfit(x, params, N = 1000, border=0.05):
+    """Plots a polynome which was fitted. 
+    x: the original x value which was fitted
+    params: the parameters in the polynome
+    N: Number of x_values to calculate
+    border: percentage of x_range to make a border"""
+    dx = x[-1] - x[0]
+    x_fit = np.linspace(x[0] - dx*border, x[-1] + dx*border, N)
+    y_fit = np.zeros(len(x_fit))
+    deg = len(params)
+    for i in range(deg):
+        y_fit += params[deg-1-i] * x_fit**i
+    
+    return (x_fit, y_fit)
+
+
 units = ["meter", "seconds", "kilogram", "kelvin", "celsius"]
 
-__all__ = ["plt", "TableHandler", "np", "ufloat", "unp", "unit", "noms", "stds", "const"]
+__all__ = ["plt", "TableHandler", "np", "ufloat", "unp", "unit", "noms", "stds", "const", "polyplotfit", "genfromtxt"]
 __all__ += units
--- tablehandler.py
+++ tablehandler.py
@@ -2,79 +2,101 @@ import numpy as np
 import uncertainties.unumpy as unp
 from uncertainties import ufloat
 
+def __parseWord(word):
+    """Parse a word from a tablefile"""
+    word = word.strip()
+    if len(word) == 0:
+        return None
+    if word[-1] == '"' and word[0] == '"':
+        return word[1:-1]
 
-class TableHandler:
-    """ Class to handle all tables in the internship """
+    try:
+        ret = float(word)
+        return float(ret)
+    except:
+        return word
+    
 
-    def __init__(self, filename):        
-        """initialisation, put the filename here!"""
-        # Open file and get lines
-        with open(filename) as f:
-            self.lines = f.readlines()
-            f.close()
-        
-        # Parse table
-        self.table = []
-        self.collumns = []
-        for line in self.lines:
-            line = line.rstrip()
+def genfromtxt(filename):
+    """Generates a Tablehandler out of the table in a given file"""
 
-            indexH = line.find("#")
-            if indexH != -1:
-                line = line[:indexH]
 
-            # Determine position of collumns
-            if len(self.collumns) == 0:
-                if line.strip() == "":
-                    continue
+    # Open file and get lines
+    with open(filename) as f:
+        lines = f.readlines()
+        f.close()
+    
+    # Parse table
+    table = []
+    collumns = []
+    for line in lines:
+        line = line.rstrip()
 
-                self.collumns = [0]
-                foundFirstArg = False
-                inArg = False
-                inQuotes = False
+        indexH = line.find("#")
+        if indexH != -1:
+            line = line[:indexH]
 
-                for i in range(len(line)):
-                    if line[i] == '#' or line[i] == "\n" or line[i] == "\f":
-                        break
+        # Determine position of collumns
+        if len(collumns) == 0:
+            if line.strip() == "":
+                continue
 
-                    if foundFirstArg == False:
-                        if line[i] != " ":
-                            foundFirstArg = True
-                            inArg = True
-                            if line[i] == '"':
-                                inQuotes = True
-                    elif inQuotes:
-                        if line[i] == '"':
-                            inQuotes = False
-                            inArg = False
-                    elif inArg == False and line[i] != " ":
-                        self.collumns.append(i)
+            collumns = [0]
+            foundFirstArg = False
+            inArg = False
+            inQuotes = False
+
+            for i in range(len(line)):
+                if line[i] == '#' or line[i] == "\n" or line[i] == "\f":
+                    break
+
+                if foundFirstArg == False:
+                    if line[i] != " ":
+                        foundFirstArg = True
                         inArg = True
                         if line[i] == '"':
-                            inQuotes = True     
-                    elif inArg and line[i] == " ":
+                            inQuotes = True
+                elif inQuotes:
+                    if line[i] == '"':
+                        inQuotes = False
                         inArg = False
+                elif inArg == False and line[i] != " ":
+                    collumns.append(i)
+                    inArg = True
+                    if line[i] == '"':
+                        inQuotes = True     
+                elif inArg and line[i] == " ":
+                    inArg = False
 
-                for i in self.collumns:
-                    self.table.append(list())
+            for i in collumns:
+                table.append(list())
 
-            found_last = False
-            for i in range(len(self.collumns)-1):
-                if(len(line) <= self.collumns[i+1]):
-                    newWord = self.__parseWord( line[self.collumns[i]:] )
-                    self.table[i].append( newWord )
-                    found_last = True
-                    # Fill table with nones so we get a rectangular shape
-                    for j in range(i+1, len(self.collumns)):
-                        self.table[j].append( None )
-                    break
-                newWord = self.__parseWord(line[self.collumns[i]:self.collumns[i+1]])
-                
-                self.table[i].append(newWord)
-        
-            if found_last == False:
-                newWord = self.__parseWord(line[self.collumns[-1]:])
-                self.table[-1].append(newWord)
+        found_last = False
+        for i in range(len(collumns)-1):
+            if(len(line) <= collumns[i+1]):
+                newWord = __parseWord( line[collumns[i]:] )
+                table[i].append( newWord )
+                found_last = True
+                # Fill table with nones so we get a rectangular shape
+                for j in range(i+1, len(collumns)):
+                    table[j].append( None )
+                break
+            newWord = __parseWord(line[collumns[i]:collumns[i+1]])
+            
+            table[i].append(newWord)
+    
+        if found_last == False:
+            newWord = __parseWord(line[collumns[-1]:])
+            table[-1].append(newWord)
+
+    return TableHandler(table)
+
+class TableHandler:
+    """ Class to handle tables in the internship easily """
+
+    def __init__(self, table = None):        
+        """Initalize table, eather from given table or from Filename"""
+        self.table = table
 
     def getDict(self):
         """Retuns a dictionary mapping the first line to its collumns"""
@@ -82,6 +104,39 @@ class TableHandler:
         for c in self.table:
             dicti[c[0]] = c[1:]
         return dicti
+
+    def getTransposedTable(self, table = None):
+        """Returns the transposition of the (underlaying, if table = None) table."""
+        if table == None:
+            table = self.table
+
+        tTable = []
+        for i in table:
+            while len(i) > len(tTable):
+                tTable.append([])
+
+        for c in table:
+            for i in range(len(c)):
+                tTable[i].append(c[i])
+
+        return tTable
+
+    def transpose(self):
+        """Transposes underlaying table"""
+        self.table = self.getTransposedTable()
+
+    def insertRow(self, row, index = None ):
+        """Add a segment to the table
+        row: 1d Table to  add
+        index: index to add the table to. Set to None to append"""
+        if index == None:
+            self.table.append(row)
+        else:
+            self.table.insert(index, row)
+    
+    def popRow(self, index=-1):
+        """Deletes row with index index"""
+        self.table.pop(index)
 
     def getRawDict(self):
         """Returns a dictionary mapping the first line to its non-string collumn values"""
@@ -95,7 +150,7 @@ class TableHandler:
         return dicti
 
     def getRawData(self):
-        """Returns only the non-strings"""
+        """Returns only floats and ints"""
         data = []
         for i in self.table:
             data.append([])
@@ -105,18 +160,12 @@ class TableHandler:
                     data[i].append(w)
         return data
 
-    def makeTexTable(self, filename):
+    def makeTexTable(self, filename, useSIUnitX=False, precision=None):
         """Generates a .tex table into file"""
         
         # Generate transposed table
-        tTable = []
-        for i in self.table:
-            while len(i) > len(tTable):
-                tTable.append([])
-        for c in self.table:
-            for i in range(len(c)):
-                tTable[i].append(c[i])
-        
+        tTable = self.getTransposedTable()
+
         # Search length and replace Nones
         maxLen = 0
         for i in range(len(tTable)):
@@ -126,13 +175,43 @@ class TableHandler:
                 if maxLen < len(str(tTable[i][j])):
                     maxLen = len(str(tTable[i][j]))
 
+        # account for SIUnitX stuff
+        maxLen += 10 
+
         # Write TeX
         file = open(filename, "w+")                
         for line in tTable:
+
             for w in line[:-1]:
-                file.write(str(w)+(maxLen - len(str(w)))*" ")
+                newWord = ""
+                if type(w)==float or type(w)==int or type(w) == ufloat:
+                    if precision != None:
+                        newWord = ("{:."+str(int(precision))+"g}").format(w)
+                    else:
+                        newWord = str(w)
+                    if useSIUnitX == True:
+                        newWord = r"\num{"+newWord+"}"
+                else:
+                    newWord = str(w)
+
+                file.write(newWord+(maxLen - len(newWord))*" ")
+                
                 file.write(" & ")
-            file.write(str(line[-1])+r"  \\"+"\n")            
+
+            newWord = ""
+            w = line[-1]
+            if type(w)==float or type(w)==int or type(w) == ufloat:
+                if precision != None:
+                    newWord = ("{:."+str(int(precision))+"g}").format(w)
+                else:
+                    newWord = str(w)
+                if useSIUnitX == True:
+                    newWord = r"\num{"+newWord+"}"
+            else:
+                newWord = str(w)
+
+            file.write(newWord+(maxLen - len(newWord))*" ")
+            file.write(r"  \\"+"\n")        
         file.close()
 
     def getMeanValues(self):
@@ -142,18 +221,3 @@ class TableHandler:
         for v in rawDict.items():
             meanValues[v[0]] = np.mean(ufloat(np.mean(v[1]), np.std(v[1])))
         return meanValues
-
-    # Parse a Word
-    def __parseWord(self, word):
-        word = word.strip()
-        if len(word) == 0:
-            return None
-        if word[-1] == '"' and word[0] == '"':
-            return word[1:-1]
-
-        try:
-            ret = float(word)
-            return float(ret)
-        except:
-            return word
-    
