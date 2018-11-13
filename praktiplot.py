c96c3cba2cefee97aaa0e8f952df5720873c2028
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2018-11-13 17:37:00 +0100

    [fix] except locale settings

--- praktiplot.py
+++ praktiplot.py
@@ -13,5 +13,9 @@ matplotlib.rcParams.update({
     'pgf.preamble': r'\input{'+dirname(abspath(__file__))+r'//matplotlib_header.tex'+r'}',
 })
 # use german locale settings for printing 3.4 as 3,4
-locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
+try:
+    locale.setlocale(locale.LC_ALL, 'de_DE.UTF8')
+except locale.Error:
+    print("Could not set the language settings! 3.5 will not be written as 3,5! SO SAD!")
+    
 plt.ticklabel_format(useLocale=True)
